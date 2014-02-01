4f4daacb7b466b69d91eb4043c88d94eb41d8f92
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-02-01 15:30:37 +1100

    Initial solution structure

---
+++ NLog.Extensions.AzureTableStorage/AzureTableStorageTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NLog;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Extensions.AzureTableStorage
+{
+    [Target("AzureTableStorage")]
+    public class AzureTableStorageTarget : TargetWithLayout 
+    {
+        [Required] 
+        public string ConnectionString { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+           //TODO: do the writing to table storage
+           
+        }
+    }
+}
