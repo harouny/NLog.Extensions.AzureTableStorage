6e26ca4fd1bdba4e1257a984b18b546235ef25d5
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-02-02 12:29:00 +1100

    Update Assemply Information

--- NLog.Extensions.AzureTableStorage/Properties/AssemblyInfo.cs
+++ NLog.Extensions.AzureTableStorage/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.Extensions.AzureTableStorage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Azure Table Storage Target for NLog")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("harouny")]
 [assembly: AssemblyProduct("NLog.Extensions.AzureTableStorage")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
