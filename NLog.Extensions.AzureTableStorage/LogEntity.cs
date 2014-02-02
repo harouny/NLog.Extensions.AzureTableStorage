3328d4c87076543b9db47987696e4002a0e43244
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-02-02 23:19:36 +1100

    Lock LogEntity Creation and Only use Time as Row Key in Table Storage

--- NLog.Extensions.AzureTableStorage/LogEntity.cs
+++ NLog.Extensions.AzureTableStorage/LogEntity.cs
@@ -6,34 +6,40 @@ namespace NLog.Extensions.AzureTableStorage
 {
     public class LogEntity : TableEntity
     {
+        private readonly object _syncRoot = new object();
+
         public LogEntity(LogEventInfo logEvent, string layoutMessage)
         {
-            LoggerName = logEvent.LoggerName;
-            LogTimeStamp = logEvent.TimeStamp.ToString(CultureInfo.InvariantCulture);
-            Level = logEvent.Level.Name;
-            Message = logEvent.FormattedMessage;
-            MessageWithLayout = layoutMessage;
-            if (logEvent.Exception != null)
+            lock (_syncRoot)
             {
-                Exception = logEvent.Exception.ToString();
-                if (logEvent.Exception.InnerException != null)
+                LoggerName = logEvent.LoggerName;
+                LogTimeStamp = logEvent.TimeStamp.ToString("g");
+                Level = logEvent.Level.Name;
+                Message = logEvent.FormattedMessage;
+                MessageWithLayout = layoutMessage;
+                if (logEvent.Exception != null)
                 {
-                    InnerException = logEvent.Exception.InnerException.ToString();
+                    Exception = logEvent.Exception.ToString();
+                    if (logEvent.Exception.InnerException != null)
+                    {
+                        InnerException = logEvent.Exception.InnerException.ToString();
+                    }
                 }
-            }
-            if (logEvent.Parameters != null)
-            {
-                Parameters = logEvent.Parameters
-                .Select(o => o.ToString())
-                .Aggregate((o, o1) => (o == null ? "Null" : o.ToString(CultureInfo.InvariantCulture)) + ", " + (o1 == null ? "Null" : o1.ToString(CultureInfo.InvariantCulture)));
+                if (logEvent.Parameters != null)
+                {
+                    Parameters = logEvent.Parameters
+                    .Select(o => o.ToString())
+                    .Aggregate((o, o1) => (o == null ? "Null" : o.ToString(CultureInfo.InvariantCulture)) + ", " + (o1 == null ? "Null" : o1.ToString(CultureInfo.InvariantCulture)));
 
+                }
+                if (logEvent.StackTrace != null)
+                {
+                    StackTrace = logEvent.StackTrace.ToString();
+                }
+                RowKey = logEvent.TimeStamp.Ticks.ToString(CultureInfo.InvariantCulture);
+                PartitionKey = LoggerName;
             }
-            if (logEvent.StackTrace != null)
-            {
-                StackTrace = logEvent.StackTrace.ToString();
-            }
-            RowKey = LoggerName + "-" + logEvent.TimeStamp.Ticks;
-            PartitionKey = LoggerName;
+            
         }
 
         public LogEntity()
